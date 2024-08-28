1ab25c1c67b265f5c7e4928517bf7c22bbb1a7fe
theMagnesium12 <mehdighorashi83@gmail.com>
2024-08-28 18:22:17 +0330

    updated preprocess.py. added methods to process insurances, expertise, badges, centers, and actions

--- preprocess.py
+++ preprocess.py
@@ -2,12 +2,13 @@ import pandas as pd
 import sqlite3
 import os
 import ast
+import re
 
 class Doctor:
     def __init__(self, dataframe:pd.DataFrame) -> None:
-        self.df = dataframe
+        self.dataframe = dataframe
 
-    def correct_gender(self, sql_script_path: str) -> pd.DataFrame:
+    def correct_gender(self, sql_script_path: str) -> None:
         # get names dataset from sql script
         try:
             os.remove('data/persian_names.db')
@@ -31,28 +32,82 @@ class Doctor:
         names.dropna(inplace=True)
 
         # correct the genders
-        api_gender = self.df['gender'].copy().apply(lambda x: x if x in [1, 2] else None)
-        self.df.drop('gender', axis=1, inplace=True)
-        self.df['name'].loc[self.df['name'].isna()] = self.df['display_name'].loc[self.df['name'].isna()].str.split().str[0] # fill missing names with display_name's first word
-        self.df = pd.merge(self.df, names, how='left', on='name')
-        self.df = self.df[
+        api_gender = self.dataframe['gender'].copy().apply(lambda x: x if x in [1, 2] else None)
+        self.dataframe.drop('gender', axis=1, inplace=True)
+        self.dataframe['name'].loc[self.dataframe['name'].isna()] = self.dataframe['display_name'].loc[self.dataframe['name'].isna()].str.split().str[0] # fill missing names with display_name's first word
+        self.dataframe = pd.merge(self.dataframe, names, how='left', on='name')
+        self.dataframe = self.dataframe[
             ['id', 'display_name', 'name', 'gender', 'calculated_rate', 'rates_count', 'insurances', 'number_of_visits', 'expertise', 'display_expertise', 'display_address', 'waiting_time', 'badges', 'centers', 'actions', 'url', 'image']
         ] # reorder columns
         for fname in [' سیده', 'سیده‌', 'سادات']:
-            self.df['gender'].loc[(self.df['name'].str.contains(fname)) & (self.df['gender'].isna())] = 2
-        self.df['gender'].loc[(self.df['name'].str.contains("سید")) & (self.df['gender'].isna())] = 1
-        self.df['gender'].loc[(self.df['gender'].isna())] = api_gender.loc[(self.df['gender'].isna())]
-        return self.df
+            self.dataframe['gender'].loc[(self.dataframe['name'].str.contains(fname)) & (self.dataframe['gender'].isna())] = 2
+        self.dataframe['gender'].loc[(self.dataframe['name'].str.contains("سید")) & (self.dataframe['gender'].isna())] = 1
+        self.dataframe['gender'].loc[(self.dataframe['gender'].isna())] = api_gender.loc[(self.dataframe['gender'].isna())]
+        return
     
-    def get_badges(self) -> pd.DataFrame:
-        self.df['badges'] = self.df['badges'].apply(lambda x: [badge['title'] for badge in ast.literal_eval(x)])
-        self.df['منتخب پذیرش24'] = self.df['badges'].apply(lambda x: 1 if 'منتخب پذیرش24' in x else 0)
-        self.df['خوش برخورد'] = self.df['badges'].apply(lambda x: 1 if 'خوش برخورد' in x else 0)
-        self.df['کمترین معطلی'] = self.df['badges'].apply(lambda x: 1 if 'کمترین معطلی' in x else 0)
-        self.df.drop('badges', axis=1, inplace=True)
-        return self.df
+    def convert_str_to_list(self) -> None:
+        for col in ['insurances', 'expertise', 'badges', 'centers', 'actions']:
+            self.dataframe[col] = self.dataframe[col].apply(lambda x: ast.literal_eval(x))
+        return
+        
+    def process_badges(self) -> None:
+        self.dataframe['badges'] = self.dataframe['badges'].apply(lambda x: [badge['title'] for badge in x])
+        return
+    
+    def process_actions(self) -> None:
+
+        def online_visit(actions):
+            for action in actions:
+                if action['title'] == 'ویزیت آنلاین':
+                    return True
+            return False
+        def first_available_appointment(actions):
+            for action in actions:
+                if 'اولین نوبت' in action['top_title']:
+                    return re.findall(r'<b>.*</b>', action['top_title'])[0][3:-4]
+            return None
+        
+        self.dataframe['online_visit'] = self.dataframe['actions'].apply(online_visit)
+        self.dataframe['first_available_appointment'] = self.dataframe['actions'].apply(first_available_appointment)
+        self.dataframe.drop('actions', axis=1, inplace=True)
+
+        return
+    
+    def process_centers(self) -> None:
+        def prune(centers):
+            center_type = {
+                1: "مطب",
+                2: "بیمارستان/درمانگاه",
+                3: "کلینیک تخصصی"
+            }
+            result = []
+            for center in centers.copy():
+                if center['address'] == None and center['display_number'] == None:
+                    continue
+                elif center['name'] == 'ویزیت آنلاین پذیرش24':
+                    continue
+                else:
+                    for key in ['id', 'user_center_id', 'server_id', 'active_booking']:
+                        center.pop(key)
+                    try:
+                        center['center_type'] = center_type[center['center_type']]
+                    except KeyError:
+                        center['center_type'] = "متفرقه"
+
+            return result
+        
+        self.dataframe['centers'] = self.dataframe['centers'].apply(prune)
+        return
+    
+    def process(self, sql_script_path: str) -> pd.DataFrame:
+        self.correct_gender(sql_script_path)
+        self.convert_str_to_list()
+        self.process_badges()
+        self.process_actions()
+        self.process_centers()
+        return self.dataframe
     
 if __name__ == "__main__":
     doctor = Doctor(pd.read_csv("data/doctors.csv"))
-    df = doctor.correct_gender("data/names.sql")
-    df = doctor.get_badges()
+    df = doctor.process("data/names.sql")
+    df.to_json("data/doctors.json", orient='records', force_ascii=False)
